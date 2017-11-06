366921c7ec36dc974f710ec6c83826082bed3bf0
toshunster <a.kukhtichev@mail.ru>
2017-11-07 02:52:40 +0300

    Rule parser and cycle for email.

--- northworks3d/gmail.py
+++ northworks3d/gmail.py
@@ -2,12 +2,15 @@
 
 import os
 import sys
+import time
 
 import email
 import logging
 import imaplib
 
 from config import GMAIL_LOGIN, GMAIL_PASSWORD
+
+from rule_parser import RuleParser
 
 FORMAT = '[%(asctime)s] [%(levelname)s] - %(message)s'
 logger = logging.getLogger(__name__)
@@ -20,6 +23,13 @@ logger.setLevel(logging.DEBUG)
 IMAP_SERVER = 'imap.gmail.com'
 IMAP_PORT = '993'
 IMAP_USE_SSL = True
+LAST_UID_FILE_NAME = 'last_uid.txt'
+
+def FromAddress( email ):
+    return email in message_from
+
+def KeywordToFind( keyword ):
+    return keyword in message_body
 
 class GmailBox:
 
@@ -30,6 +40,8 @@ class GmailBox:
     
     def __enter__( self ):
         self.imap.login( self.username, self.password )
+        logger.info( "Login successfully." )
+
         return self
     
     def __exit__(self, type, value, traceback):
@@ -43,7 +55,6 @@ class GmailBox:
         #retrieves the latest (newest) email by sequential ID
         ids = data[0]
         id_list = ids.split()
-        print( ids )
         latest_email_id = id_list[-1]
 
         # Fetching the mail, "`(RFC822)`" means "get the whole stuff", but you can ask for headers only, etc.
@@ -59,6 +70,7 @@ class GmailBox:
     def get_messages_by_uid( self, uid ):
         # Issue the search command of the form "SEARCH UID 42:*" or "ALL".
         command = "UID {}:*".format( uid ) if uid is not None else "ALL"
+        logger.info( "Command: {}".format( command ) )
         self.imap.select( "inbox" )
         result, data = self.imap.search( None, command )
         messages = data[0].split()
@@ -74,43 +86,54 @@ class GmailBox:
                 yield message_uid, email.message_from_string( email_body.decode('utf-8') )
         return dict()
 
-LAST_UID_FILE_NAME = 'last_uid.txt'
-
 if __name__ == "__main__":
     if len( sys.argv ) != 2:
         print( "Usage: {0} <rules file>".format( sys.argv[0] ) )
         sys.exit(1)
     rule_file_name = sys.argv[1]
     rules = list()
+    # Read rule filename.
     with open( rule_file_name, 'r' ) as rule_file:
         for rule_line in rule_file:
             rule, script_file = rule_line.strip().split('\t')
             rules.append( ( rule, script_file ) )
         logger.info('Read {} rules.'.format( len(rules) ))
-    logger.info( "Success!" )
     uid = None
+    start_uid = None
     if os.path.isfile( LAST_UID_FILE_NAME ):
         try:
             uid = int( open( LAST_UID_FILE_NAME, 'r' ).read().strip() )
+            start_uid = uid
         except:
             logger.warning( "Can't open {} file. Start read email from begin.".format( LAST_UID_FILE_NAME ) )
     # Logs in to the desired account and navigates to the inbox.
     try:
         with GmailBox( GMAIL_LOGIN, GMAIL_PASSWORD ) as gmail:
-            #message = gmail.get_latest_message()
-            #uid = None
-            #uid = 5
-            for message_uid, message in gmail.get_messages_by_uid( uid ):
-                print( message_uid, message.get('Subject', None) )
-                uid = message_uid
+            while True:
+                for message_uid, message in gmail.get_messages_by_uid( uid ):
+                    #print( message_uid, message.get('Subject', None) )
+                    uid = int( message_uid )
+                    message_from = message.get('From', None)
+                    message_to = message.get('To', None)
+                    message_body = None
+                    for part in message.walk():
+                        if part.get_content_type() == "text/plain": # ignore attachments/html
+                            message_body = part.get_payload(decode=True).decode("utf-8")
+                            break
+                    for enum, (rule, script) in enumerate( rules ):
+                        tree = RuleParser( rule, globals() )
+                        if tree.eval_rule():
+                            logger.info( "Match {} rule. Executing '{}' script.".format( enum+1, script ) )
+                            break
+                    # Save new uid in file.
+                    if start_uid is not None and start_uid != uid:
+                        with open( LAST_UID_FILE_NAME, 'w' ) as output:
+                            output.write( str(int(uid)) )
+                time.sleep( 5 )
     except imaplib.IMAP4.error as e:
         logger.error( "imaplib.IMAP4.error: {}".format( str(e) ) )
         sys.exit(1)
     except Exception as e: 
-        logger.error( "Unknown exception: {}".format( str(e) ) )
+        logger.error( "Unknown exception: {}".format( e ) )
         sys.exit(1)
     
-    with open( LAST_UID_FILE_NAME, 'w' ) as output:
-        output.write( str(int(uid)) )
-    #print( email_message['To'] )
-    #print( email_message['From'] )
--- northworks3d/rule_parser.py
+++ northworks3d/rule_parser.py
@@ -12,84 +12,93 @@ def func1( arg1 ):
 def func2( arg1 ):
     return False
 
-def func3( arg1, arg2 ):
+def func3( arg1 ):
     return True
 
-def func4( arg1, arg2, arg3, arg4 ):
+def func4( arg1 ):
     return False
 
-def RuleParser( fpexp ):
-    fplist = fpexp.split()
-    pStack = Stack()
-    eTree = BinaryTree('')
-    pStack.push(eTree)
-    currentTree = eTree
-    is_func_args = False
-    for i in fplist:
-        # Open bracket.
-        if i == '(':
-            if is_func_args:
-                parent = pStack.pop()
-                currentTree = parent
-            currentTree.insertLeft('')
-            pStack.push( currentTree )
-            currentTree = currentTree.getLeftChild()
-            is_func_args = False
-        # Function name or arguments.
-        elif i.lower() not in [ 'and', 'or', ')' ]:
-            if not is_func_args:
-                currentTree.setRootVal( (i, []) )
-                #parent = pStack.pop()
-                #currentTree = parent
-                is_func_args = True
-                continue
-            currentTree.key[1].append( i )
-        elif i.lower() in [ 'and', 'or' ]:
-            if is_func_args:
-                parent = pStack.pop()
-                currentTree = parent
-            currentTree.setRootVal( i )
-            currentTree.insertRight('')
-            pStack.push(currentTree)
-            currentTree = currentTree.getRightChild()
-            is_func_args = False
-        elif i == ')':
-            if is_func_args:
-                parent = pStack.pop()
-                currentTree = parent
-            currentTree = pStack.pop()
-            is_func_args = False
-        else:
-            raise ValueError
-    return eTree
+class RuleParser(object):
+    tree = None
+    operators = { 'and' : operator.and_, 'or' : operator.or_ }
 
-def printexp(tree):
-    sVal = ""
-    if tree:
-        if tree.getLeftChild():
-            sVal = printexp(tree.getLeftChild())
-        sVal = sVal + str(tree.key[0] + " {}".format( " ".join( tree.key[1] ) ) if type(tree.key) is tuple else " {} ".format( tree.key ) )
-        if tree.getLeftChild():
-            sVal = sVal + printexp(tree.getRightChild())
-        if tree.getLeftChild() and tree.getLeftChild():
-            sVal = "( {} )".format( sVal )
-    return sVal
+    def __init__( self, rule, globals=globals() ):
+        tokens = rule.split()
+        pStack = Stack()
+        self.tree = BinaryTree('')
+        pStack.push( self.tree )
+        current_tree = self.tree
+        is_func_args = False
+        self.globals = globals
+        for token in tokens:
+            # Open bracket.
+            if token == '(':
+                if is_func_args:
+                    parent = pStack.pop()
+                    current_tree = parent
+                current_tree.insertLeft('')
+                pStack.push( current_tree )
+                current_tree = current_tree.getLeftChild()
+                is_func_args = False
+            # Function name or arguments.
+            elif token.lower() not in [ 'and', 'or', ')' ]:
+                if not is_func_args:
+                    current_tree.setRootVal( (token, []) )
+                    is_func_args = True
+                    continue
+                current_tree.key[1].append( token )
+            elif token.lower() in [ 'and', 'or' ]:
+                if is_func_args:
+                    parent = pStack.pop()
+                    current_tree = parent
+                current_tree.setRootVal( token )
+                current_tree.insertRight('')
+                pStack.push(current_tree)
+                current_tree = current_tree.getRightChild()
+                is_func_args = False
+            elif token == ')':
+                if is_func_args:
+                    parent = pStack.pop()
+                    current_tree = parent
+                current_tree = pStack.pop()
+                is_func_args = False
+            else:
+                raise ValueError
+        self.tree = current_tree
+    
+    def eval_rule( self ):
+        return self.eval_rule_( self.tree )
+    
+    def eval_rule_( self, tree ):
+        left_child = None
+        right_child = None
+        if tree:
+            left_child = self.eval_rule_( tree.getLeftChild() )
+            right_child = self.eval_rule_( tree.getRightChild() )
+            if left_child is not None and right_child is not None:
+                if type( left_child ) is not bool:
+                    left_child = self.globals[left_child[0]]( " ".join( left_child[1] ) )
+                if type( right_child ) is not bool:
+                    right_child = self.globals[right_child[0]]( " ".join( right_child[1] ) )
+                return self.operators[ tree.getRootVal().lower() ]( left_child, right_child )
+            else:
+                return tree.getRootVal()
+    
+    def serialize( self ):
+        return self.serialize_( self.tree )
 
-def postordereval(tree):
-    opers = { 'and' : operator.and_, 'or' : operator.or_ }
-    res1 = None
-    res2 = None
-    if tree:
-        res1 = postordereval( tree.getLeftChild() )
-        res2 = postordereval( tree.getRightChild() )
-        if res1 is not None and res2 is not None:
-            if type( res1 ) is not bool:
-                res1 = globals()[res1[0]]( *res1[1] )
-            if type( res2 ) is not bool:
-                res2 = globals()[res2[0]]( *res2[1] )
-            return opers[ tree.getRootVal().lower() ]( res1, res2 )
-        else:
-            return tree.getRootVal()
+    def serialize_( self, tree ):
+        value = ""
+        if tree:
+            if tree.getLeftChild():
+                value = self.serialize_( tree.getLeftChild() )
+            current_node_value = "{} {}".format( tree.key[0], " ".join( tree.key[1] ) ) if type(tree.key) is tuple else " {}".format( tree.key )
+            value += current_node_value
+            if tree.getLeftChild():
+                value = "{} {}".format( value, self.serialize_( tree.getRightChild() ) )
+            if tree.getLeftChild() and tree.getLeftChild():
+                value = "( {} )".format( value )
+        return value
 
 if __name__ == "__main__":
     rules = [ "( func1 arg1 AND func1 arg1 )",
@@ -97,10 +106,10 @@ if __name__ == "__main__":
               "( ( func1 arg1 AND func2 arg2 ) OR func3 arg1 arg2 )" ]
     # Check input rules are parsed correct.
     for rule in rules:
-        root = RuleParser( rule )
+        tree = RuleParser( rule )
         # Example string must match the serialized tree string.
-        result = printexp( root )
+        result = tree.serialize()
         if result != rule:
             print( "AssertionError:\n  expected: [{}],\n  actual:   [{}]".format( rule, result ) )
-    # 
-    assert postordereval( root )
+    
+    assert tree.eval_rule( )
