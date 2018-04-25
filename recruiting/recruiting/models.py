70cf46d98742f704a225976fbd75de2aeb2673d5
toshunster <a.kukhtichev@mail.ru>
2018-04-25 03:21:56 +0300

    New Recruiting project.

---
+++ recruiting/recruiting/models.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+class Company( models.Model ):
+    name = models.CharField( max_length=256, blank=Flase, null=False )
+    logo = models.CharField( max_length=512 )
+    
+    class Meta:
+        verbose_name="company"
+        verbose_name_plural="companies"
+
+class City( models.Model ):
+    name = models.CharField( max_length=256, blank=Flase, null=False )
+    
+    class Meta:
+        verbose_name="city"
+        verbose_name_plural="cities"
+
+class Vacancy( models.Model ):
+    is_active = models.BooleanField( default=False )
+    starts_at = models.DateTimeField( auto_now_add = True )a
+    title = models.CharField( max_length=256 )
+    description = models.TextField()
+    company = models.ForeignKey( Company )
+    location = models.ForeignKey( City )
+
+    class Meta:
+        verbose_name="vacancy"
+        verbose_name_plural = "vacancies"
